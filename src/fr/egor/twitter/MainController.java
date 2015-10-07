b4ddb7101aad4288c9e42e8a9ac526d9e56004d8
ChoucheneH <hedhili.chouchene@outlook.fr>
2015-10-07 23:31:35 +0200

    Afficher :Image  de Profil , nombres de abonnés, abonnements,tweets.

--- src/fr/egor/twitter/MainController.java
+++ src/fr/egor/twitter/MainController.java
@@ -1,11 +1,13 @@
 package fr.egor.twitter;
-
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
 import twitter4j.Status;
 import twitter4j.TwitterException;
 
@@ -18,19 +20,26 @@ public class MainController implements Initializable {
 
   @FXML
   public ListView<Status> tweets;
-
-
-  public void lol(ActionEvent event) {
-    System.out.println("lol");
-  }
+  public ImageView userPictureByUrl;
+  public String urlUserPicture;
+  public Label fullNameProfil,screenNameProfil,tweetsNombers,abonnementsNumbers,abonnesNumbers;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     twitterAPI = new TwitterAPI();
     try {
       tweetsList = FXCollections.observableArrayList(twitterAPI.getHomeTimeline());
-      //tweets.setCellFactory((listView, listCell) -> );
+      urlUserPicture = twitterAPI.getProfileImageUrl();
+
+      tweetsList = FXCollections.observableArrayList(twitterAPI.getHomeTimeline());
       tweets.setItems(tweetsList);
+      Image image = new Image(urlUserPicture);
+      userPictureByUrl.setImage(image);
+      fullNameProfil.setText(twitterAPI.getFullName());
+      screenNameProfil.setText("@"+twitterAPI.getFullScreenName());
+      tweetsNombers.setText(twitterAPI.getTweetsCountProfil());
+      abonnementsNumbers.setText(twitterAPI.getFriendsCountProfil());
+      abonnesNumbers.setText(twitterAPI.getFollowersCountProfil());
     } catch (TwitterException e) {
       e.printStackTrace();
     }
