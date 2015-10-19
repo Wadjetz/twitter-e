00020d9a3d8a2dfdea3684f9c8f29f2818be77b3
ChoucheneH <hedhili.chouchene@outlook.fr>
2015-10-20 00:23:07 +0200

    Modification Du TweetCell

--- src/fr/egor/twitter/view/tweet/list/TweetCell.java
+++ src/fr/egor/twitter/view/tweet/list/TweetCell.java
@@ -1,10 +1,12 @@
 package fr.egor.twitter.view.tweet.list;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import twitter4j.Status;
 
@@ -25,16 +27,27 @@ public class TweetCell extends ListCell<Status> {
       iv1.setImage(image);
 
       Label lblName = new Label(status.getUser().getName());
+      lblName.setPadding(new Insets(5,15,0,0));
       hb2.getChildren().add(lblName);
 
-      Label lblScreenName = new Label(status.getUser().getScreenName());
+
+
+      Label lblScreenName = new Label("@"+status.getUser().getScreenName());
+      lblScreenName.setPadding(new Insets(5,15,0,0));
       hb2.getChildren().add(lblScreenName);
 
+
       Label lblCreatedAt = new Label(status.getUser().getCreatedAt().toString());
+      lblCreatedAt.setPadding(new Insets(5,15,0,0));
       hb2.getChildren().add(lblCreatedAt);
 
+      Label lblStatus = new Label(status.getText());
+      vb1.getChildren().add(hb2);
+      vb1.getChildren().add(lblStatus);
+
+
       hb1.getChildren().add(iv1);
-      hb1.getChildren().add(hb2);
+      hb1.getChildren().add(vb1);
 
       setGraphic(hb1);
     }
